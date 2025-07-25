33cbf17e222c6d87b5d1ce889718e3019690e4ce
Hafizhmu <hafizhktb@gmail.com>
2025-07-25 18:21:04 +0800

    Stats Card Dashboard

--- app/routes/admin/all-user.tsx
+++ app/routes/admin/all-user.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import { Header } from "components";
 
 const AllUser = () => {
   return (
-  <div>AllUser</div>
+    <main className="dashboard wrapper">
+      <Header
+        title="Trips Page"
+        description="Check out our current users, trends and popular destinations in real time"
+      />
+      All Users Page COntents
+    </main>
   );
 };
 
